775f77eb89cb82f2cab8b726524203eeebe72649
amphioxis <paulingm@hu-berlin.de>
2020-11-21 11:33:24 +0100

    switch to official packages (for server) only

--- server.go
+++ server.go
@@ -1,12 +1,26 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+    "net/http"
+    "fmt"
+)
+
+func queryOutput(response http.ResponseWriter, request *http.Request) {
+    fmt.Print("request: ")
+    fmt.Println(request)
+    fmt.Println("response: ")
+    fmt.Println(response)
+}
 
 func main() {
-  m := martini.Classic()
-  m.Get("/", func() string {
-    return "Hallo Welt!"
-  })
-  m.RunOnAddr(":8080")
-  m.Run()
+    http.HandleFunc("/helloworld", func(response http.ResponseWriter, request *http.Request) {
+        queryOutput(response, request)
+    })
+    http.ListenAndServe(":8080", nil)
 }
+
+
+
+
+
+// &{GET /helloworld HTTP/1.1 1 1 map[Accept-Encoding:[gzip] User-Agent:[Go-http-client/1.1]] {} <nil> 0 [] false localhost:8080 map[] map[] <nil> map[] [::1]:35960 /helloworld <nil> <nil> <nil> 0xc0001180c0}
