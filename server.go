2ba41df75ba075eb73f9ebbbaa4147de8492595c
amphioxis <paulingm@hu-berlin.de>
2020-11-22 21:28:27 +0100

    unused output commented out, handle empty value, general output fixed

--- server.go
+++ server.go
@@ -30,31 +30,41 @@ func queryOutput(toEdit string, response http.ResponseWriter, request *http.Requ
 
 func editRequest(toEdit string, u Url) (Url) {
 
-   fmt.Println("enter editRequest")
-   fmt.Println("toEdit: ")
-   fmt.Println(toEdit)
+//   fmt.Println("enter editRequest")
+//   fmt.Println("toEdit: ")
+//   fmt.Println(toEdit)
 
     re := regexp.MustCompile(`/.* `) // picks the path with query of GET /path?key=value HTTP/1.1
 //    fmt.Printf("%q\n", re.FindString(toEdit))
     toEdit = re.FindString(toEdit)
-   fmt.Println("path with query:", toEdit)
+//   fmt.Println("path with query:", toEdit)
     re = regexp.MustCompile(`/.[^\?]*`) // picks the path of /path?key=value
     u.path = re.FindString(toEdit)
-   fmt.Println("path:", u.path + "!")
+//   fmt.Println("path:", u.path + "!")
     u.path = strings.Replace(u.path, " ", "", -1)
 //    fmt.Println("path:", u.path + "!")
    fmt.Println("path:", u.path + "!")
 
-    re = regexp.MustCompile(`\?(.*?)\=`) //picks ?key=
+//    re = regexp.MustCompile(`\?(.*?)\=`) //picks ?key=
+    re = regexp.MustCompile(`\?.*`) //picks ?key=value
     u.key = re.FindString(toEdit)
-   fmt.Println("key:", u.key) // ?name=
-    var toCut string = u.path + u.key //
-   fmt.Println("toCut:", toCut) // /path?key=
-    u.value = strings.TrimPrefix(toEdit, toCut) // picks value of /path?key=value
-   fmt.Println("value:", u.value)
+   fmt.Println("key:", u.key) // ?name=value
+
+    re = regexp.MustCompile(`\=.*`) //picks ?key=value
+    u.value = re.FindString(u.key)
+   fmt.Println("value1:", u.value) // =value
+
+
+//    var toCut string = u.path + u.key //
+//   fmt.Println("toCut:", toCut) // /path?key=
+//    u.value = strings.TrimPrefix(toEdit, toCut) // picks value of /path?key=value
+    u.key = strings.TrimSuffix(u.key, u.value) // ?name
     u.key = strings.TrimPrefix(u.key, "?")
-    u.key = strings.TrimSuffix(u.key, "=")
-//    fmt.Println("u.key:", u.key)
+    u.key = strings.Replace(u.key, " ", "", -1)
+    u.value = strings.TrimPrefix(u.value, "=")
+    u.value = strings.Replace(u.value, " ", "", -1)
+   fmt.Println("key:", u.key)
+   fmt.Println("value:", u.value)
 
    fmt.Println("exit editRequest")
 		return u
@@ -80,7 +90,10 @@ func camelCaseToSpace(u Url) (string) {
     }
   }
 
-  fmt.Println(responseValue)
+  responseValue = strings.TrimSuffix(responseValue, " ")
+  responseValue = "\"" + responseValue + "\""
+
+//  fmt.Println(responseValue)
   return responseValue
 }
 
@@ -91,10 +104,14 @@ func sendResponse(u Url, responseValue string, response http.ResponseWriter, req
 //    fmt.Println("u.path2:", u.path)
     if u.key == "" {
 //    fmt.Println("u.key:", u.key)
-      io.WriteString(response, "Hello Stranger")
+      io.WriteString(response, "\"Hello Stranger\"")
     } else if u.key == "name" {
 //    fmt.Println("u.key:", u.key)
-      io.WriteString(response, responseValue)
+      if u.value != ""{
+        io.WriteString(response, responseValue)
+      } else {
+        io.WriteString(response, "no value")
+      }
     } else {
       io.WriteString(response, "unkown key")
     }
