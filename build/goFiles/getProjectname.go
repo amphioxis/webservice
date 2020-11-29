002371caf7bb96a6098c7b25a57343ea1041aa8c
amphioxis <paulingm@hu-berlin.de>
2020-11-29 23:53:53 +0100

    add documentation to 'getProjectname'

--- build/goFiles/getProjectname.go
+++ build/goFiles/getProjectname.go
@@ -1,3 +1,7 @@
+/* This function parses the projectname out of the given repository url.
+ * Also works with ssh.
+ */
+
 package main
 
 import (
@@ -7,14 +11,10 @@ import (
 
 func getProjectname(g Git) (Git) {
 
-//  fmt.Println("enter getProjectname")
-//  fmt.Println(g.projectname)
   re := regexp.MustCompile(`.*/`)
   var toCut string = re.FindString(g.Projectname)
   g.Projectname = strings.TrimPrefix(g.Projectname, toCut)
-//  fmt.Println(g.projectname)
   g.Projectname = strings.TrimSuffix(g.Projectname, ".git")
-//  fmt.Println(g.projectname)
 
   return g
 }
